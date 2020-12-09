d6bdfc2dcb3021774fb87f10497a0ed571c6941d
jomapormentilla <jomapormentilla@gmail.com>
2020-12-09 17:41:14 -0500

    Update Signup Form to include Regex for Email character validation using RFC 5322 Official Standard.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -44,7 +44,13 @@ class ApplicationController < Sinatra::Base
         redirect_if_form_empty( params[:wizard] )
 
         if params[:wizard][:username].split.any?{ |char| char =~ /\W/ }
-            flash[:message] = "Error: Username contains invalid characters."
+            flash[:message] = "Error: Username <b>#{ params[:wizard][:username] }</b> contains invalid characters."
+            flash[:alert_type] = "danger"
+            redirect '/signup'
+        end
+
+        if valid_email_regex?(params[:wizard][:email])
+            flash[:message] = "Error: Email Address <b>#{ params[:wizard][:email] }</b> contains invalid characters."
             flash[:alert_type] = "danger"
             redirect '/signup'
         end
@@ -105,6 +111,11 @@ class ApplicationController < Sinatra::Base
         def parse_timestamp( time )
             _time = time.in_time_zone('Eastern Time (US & Canada)')
             _time.strftime("%B %d, %Y, %l:%M%P")
+        end
+
+        def valid_email_regex?( string )
+            # General Email Regex (RFC 5322 Official Standard)
+            string.match(/(?:[a-z0-9!#$%&'*+\/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+\/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/)
         end
     end
 
